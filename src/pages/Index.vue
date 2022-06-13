b84600599d98f6262a8596569ab1c4786b112588
isrphilipe <israelphilipeassuncao@gmail.com>
2022-06-12 22:58:33 -0300

    adding contact info

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="flex">
     <div class="q-pa-lg full-width text-bold">
-      <q-card-section>
+      <div>
         <div class="hello text-h2 q-my-xl">Hello there</div>
-      </q-card-section>
-      <q-card-section>
+      </div>
+      <div>
         <div class="text-h4">
           <div>
             My name is Israel Philipe, i am a 24 years old software developer.
@@ -22,8 +22,24 @@
             developer and tech leader on a team of 3 (myself included).
           </div>
         </div>
-        <div class="text-h4"></div>
-      </q-card-section>
+      </div>
+      <q-separator class="q-ma-md q-my-xl"></q-separator>
+      <div class="text-h2 q-my-lg">Contact information</div>
+      <div class="text-h5 q-my-lg">
+        If you are interested you can contact me using the information bellow.
+        <br />
+        Email and whatsapp are my favorite options
+      </div>
+      <div class="text-h5">
+        <div>Email : israelphilipeassuncao@gmail.com</div>
+        <div>Phone : +55 84 998104581</div>
+        <div>
+          Github :
+          <a href="https://github.com/israelphilipe"
+            >https://github.com/israelphilipe</a
+          >
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
